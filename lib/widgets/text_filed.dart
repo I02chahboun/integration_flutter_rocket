c14990e6ebfae595bbf3afbc32a7197748ed543b
I02chahboun <ismail02.dev@gmail.com>
2023-07-17 19:47:01 +0200

    add obscure text

--- lib/widgets/text_filed.dart
+++ lib/widgets/text_filed.dart
@@ -3,20 +3,33 @@ import 'package:integration_flutter_rocket/constants/texts.dart';
 
 class TextFielld extends StatelessWidget {
   final String labelText;
-  const TextFielld({super.key, required this.labelText});
-
+  TextFielld({super.key, required this.labelText});
+  final ValueNotifier<bool> isVisibilty = ValueNotifier(true);
   @override
   Widget build(BuildContext context) {
     final bool isPassword = labelText == AppTexts.labelPassword;
-    return TextField(
-      decoration: InputDecoration(
-          border: const OutlineInputBorder(),
-          labelText: labelText,
-          suffixIcon: isPassword ? const Icon(Icons.visibility) : null,
-          prefixIcon: Icon(
-            isPassword ? Icons.password : Icons.email,
-            size: 22,
-          )),
-    );
+    return ValueListenableBuilder(
+        valueListenable: isVisibilty,
+        builder: (context, value, _) {
+          return TextField(
+            obscureText: isPassword ? isVisibilty.value : false,
+            decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                labelText: labelText,
+                suffixIcon: isPassword
+                    ? IconButton(
+                        onPressed: () {
+                          isVisibilty.value = !isVisibilty.value;
+                        },
+                        icon: Icon(isVisibilty.value
+                            ? Icons.visibility
+                            : Icons.visibility_off))
+                    : null,
+                prefixIcon: Icon(
+                  isPassword ? Icons.password : Icons.email,
+                  size: 22,
+                )),
+          );
+        });
   }
 }
