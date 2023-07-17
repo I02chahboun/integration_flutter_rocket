cd709cdc25cb36f25d7d4d71e26e8608be7146ec
I02chahboun <ismail02.dev@gmail.com>
2023-07-17 19:45:48 +0200

    add constants

---
+++ lib/constants/image.dart
@@ -0,0 +1,3 @@
+class AppAssets {
+  static const String vectorLogin = "assets/vector_login.png";
+}
--- lib/constants/texts.dart
+++ lib/constants/texts.dart
@@ -1,4 +1,7 @@
 class AppTexts {
   static const String labelEmail = "email";
   static const String labelPassword = "password";
+  static const String titleLogin = "Login";
+  static const String heveAccount = "Don’t have an account?";
+  static const String signUp = "Singup";
 }
