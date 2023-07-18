81bb26d66dc58b719630b3ec9ff3fe9042ea1d7c
I02chahboun <ismail02.dev@gmail.com>
2023-07-18 18:20:03 +0200

    disable auto leading & modifier preferred size

--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:integration_flutter_rocket/constants/texts.dart';
 
 class AppBarr extends StatelessWidget implements PreferredSizeWidget {
   final BuildContext context;
   const AppBarr(this.context, {super.key});
   @override
   Size get preferredSize => Size(MediaQuery.of(context).size.width,
-      MediaQuery.of(context).size.height * 0.07);
+      MediaQuery.of(context).size.height * 0.06);
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      automaticallyImplyLeading: false,
+      title: const Text(
+        AppTexts.home,
+        style: TextStyle(fontWeight: FontWeight.w700),
+      ),
+      centerTitle: true,
       actions: [
         IconButton(
           onPressed: () {},
