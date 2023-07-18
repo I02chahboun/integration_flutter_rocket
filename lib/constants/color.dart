ddcb57ecd1909a29274f02095bdbbbc1599a6886
I02chahboun <ismail02.dev@gmail.com>
2023-07-18 18:13:20 +0200

    add colors

--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color green = Color(0xff5DFF00);
+  static const Color green = Color(0xff008000);
+  static const Color grey = Color(0xff808080);
 }
