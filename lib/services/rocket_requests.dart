9900effa97bc45beb493922dfd66ac67f83286bf
I02chahboun <ismail02.dev@gmail.com>
2023-07-19 14:52:09 +0200

    home integration api

---
+++ lib/services/rocket_requests.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_rocket/flutter_rocket.dart';
+import 'package:integration_flutter_rocket/constants/api.dart';
+import 'package:integration_flutter_rocket/models/product.dart';
+
+class Request {
+  static targetData(data) => ["products"];
+  static Future get(Product product) {
+    return Rocket.get<RocketClient>("get")
+        .request(Api.product, model: product, inspect: targetData);
+  }
+}
