cd697a5afcba59022615c6522eecd2070ab0a565
I02chahboun <ismail02.dev@gmail.com>
2023-07-19 14:55:21 +0200

    remove const

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:integration_flutter_rocket/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
