a568184f573b43a5cd48ad5f2bd92da505456704
I02chahboun <ismail02.dev@gmail.com>
2023-07-19 16:41:55 +0200

    handle integration get

--- lib/services/rocket_requests.dart
+++ lib/services/rocket_requests.dart
@@ -3,7 +3,7 @@ import 'package:integration_flutter_rocket/constants/api.dart';
 import 'package:integration_flutter_rocket/models/product.dart';
 
 class Request {
-  static targetData(data) => ["products"];
+  static targetData(data) => data["products"];
   static Future get(Product product) {
     return Rocket.get<RocketClient>("get")
         .request(Api.product, model: product, inspect: targetData);
