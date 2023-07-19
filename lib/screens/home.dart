a9e74ccc5273cbcbc908f06d5f1b3679a3ceac4f
I02chahboun <ismail02.dev@gmail.com>
2023-07-19 14:53:23 +0200

    integration api home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rocket/flutter_rocket.dart';
+import 'package:integration_flutter_rocket/models/product.dart';
+import 'package:integration_flutter_rocket/services/rocket_requests.dart';
 import 'package:integration_flutter_rocket/widgets/appbar.dart';
 import 'package:integration_flutter_rocket/widgets/card_product.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  final Product product = Product();
+  Home({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,20 +15,28 @@ class Home extends StatelessWidget {
       appBar: AppBarr(context),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: GridView.builder(
-          itemCount: 3,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, mainAxisSpacing: 10, crossAxisSpacing: 10),
-          itemBuilder: (context, index) {
-            return const CardProduct(
-              image:
-                  "https://mobizil.com/wp-content/uploads/2020/02/Samsung-Galaxy-S20.jpg",
-              title: "Phone S20",
-              description: "the is Phone S20",
-              price: 30.0,
-            );
-          },
-        ),
+        child: RocketView<Product>(
+            call: () => Request.get(product),
+            model: product,
+            loader: const CircularProgressIndicator(),
+            builder: (context, state) {
+              return GridView.builder(
+                itemCount: product.all!.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10),
+                itemBuilder: (context, index) {
+                  final Product products = product.all![index];
+                  return CardProduct(
+                    image: products.thumbnail!,
+                    title: products.title!,
+                    description: products.description!,
+                    price: products.price!,
+                  );
+                },
+              );
+            }),
       ),
     );
   }
