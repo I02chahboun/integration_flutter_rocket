cc38f2ea1494e3d1d56b6d5d157d4cc1ca5e3cff
I02chahboun <ismail02.dev@gmail.com>
2023-07-19 17:26:51 +0200

    add integration login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rocket/flutter_rocket.dart';
+import 'package:integration_flutter_rocket/constants/api.dart';
 import 'package:integration_flutter_rocket/constants/image.dart';
 import 'package:integration_flutter_rocket/constants/texts.dart';
 import 'package:integration_flutter_rocket/constants/util.dart';
+import 'package:integration_flutter_rocket/models/auth.dart';
 import 'package:integration_flutter_rocket/screens/home.dart';
 import 'package:integration_flutter_rocket/widgets/button.dart';
 import 'package:integration_flutter_rocket/widgets/text_filed.dart';
 import 'package:integration_flutter_rocket/widgets/title_auth.dart';
 
 class LogIn extends StatelessWidget {
-  const LogIn({super.key});
+  final Post post = Post();
+  LogIn({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -47,5 +51,27 @@ class LogIn extends StatelessWidget {
         ),
       ),
     ));
+  }
+
+  void _post(BuildContext context) {
+    Map<String, dynamic> data = {
+      "email": "Developer5@gmail.com",
+      "password": 123456,
+    };
+    Rocket.get<RocketClient>("post").request(Api.login,
+        data: data,
+        model: post,
+        method: HttpMethods.post,
+        params: {
+          'Authorization': 'Bearer <token>'
+        }).whenComplete(() => _validLogin(context));
+  }
+
+  void _validLogin(BuildContext context) {
+    if (post.existData) {
+      context.push(Home());
+    } else {
+      post.exception.response;
+    }
   }
 }
