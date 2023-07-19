2eeae0cb0335249c29267cd8ee25b42edfca82db
I02chahboun <ismail02.dev@gmail.com>
2023-07-19 18:15:43 +0200

    improved parameters

--- lib/widgets/card_product.dart
+++ lib/widgets/card_product.dart
@@ -1,46 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:integration_flutter_rocket/constants/color.dart';
+import 'package:integration_flutter_rocket/constants/util.dart';
+import 'package:integration_flutter_rocket/models/product.dart';
+import 'package:integration_flutter_rocket/screens/product_details.dart';
 
 class CardProduct extends StatelessWidget {
-  final String image, title, description;
-  final int price;
-  const CardProduct(
-      {super.key,
-      required this.image,
-      required this.title,
-      required this.description,
-      required this.price});
+  final Product product;
+  const CardProduct({super.key, required this.product});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(),
-        borderRadius: BorderRadius.circular(3.0),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child:
-            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-          Expanded(child: Image(image: NetworkImage(image))),
-          Text(
-            title,
-            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-            overflow: TextOverflow.ellipsis,
-          ),
-          Text(
-            description,
-            style: const TextStyle(fontSize: 14, color: AppColors.grey),
-            overflow: TextOverflow.ellipsis,
-          ),
-          Text(
-            "price: $price\$",
-            style: const TextStyle(
-                fontSize: 17,
-                color: AppColors.green,
-                fontWeight: FontWeight.w700),
-          ),
-        ]),
+    return GestureDetector(
+      onTap: () {
+        context.push(ProductDetails(
+          product: product,
+        ));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(),
+          borderRadius: BorderRadius.circular(3.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            Expanded(child: Image(image: NetworkImage(product.thumbnail!))),
+            Text(
+              product.title!,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              product.description!,
+              style: const TextStyle(fontSize: 14, color: AppColors.grey),
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              "price: ${product.price}\$",
+              style: const TextStyle(
+                  fontSize: 17,
+                  color: AppColors.green,
+                  fontWeight: FontWeight.w700),
+            ),
+          ]),
+        ),
       ),
     );
   }
