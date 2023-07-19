f74ca2ec0619fbcc404d36490fdd1e2937a67f97
I02chahboun <ismail02.dev@gmail.com>
2023-07-19 18:16:47 +0200

    improved parameters

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,38 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rocket/flutter_rocket.dart';
+import 'package:integration_flutter_rocket/constants/texts.dart';
 import 'package:integration_flutter_rocket/models/product.dart';
 import 'package:integration_flutter_rocket/services/rocket_requests.dart';
 import 'package:integration_flutter_rocket/widgets/appbar.dart';
 import 'package:integration_flutter_rocket/widgets/card_product.dart';
 
 class Home extends StatelessWidget {
-  final Product product = Product();
+  final Product products = Product();
   Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarr(context),
+      appBar: AppBarr(
+        context,
+        title: AppTexts.home,
+      ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: RocketView<Product>(
-            call: () => Request.get(product),
-            model: product,
+            call: () => Request.get(products),
+            model: products,
             loader: const CircularProgressIndicator(),
             builder: (context, state) {
               return GridView.builder(
-                itemCount: product.all!.length,
+                itemCount: products.all!.length,
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 2,
                     mainAxisSpacing: 10,
                     crossAxisSpacing: 10),
                 itemBuilder: (context, index) {
-                  final Product products = product.all![index];
+                  final Product product = products.all![index];
                   return CardProduct(
-                    image: products.thumbnail!,
-                    title: products.title!,
-                    description: products.description!,
-                    price: products.price!,
+                    product: product,
                   );
                 },
               );
