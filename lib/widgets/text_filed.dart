8dfbb5d9421a2f4adcff645659d9a7d6c973b42d
I02chahboun <ismail02.dev@gmail.com>
2023-07-20 14:44:20 +0200

    add parameter validate & rocket value

--- lib/widgets/text_filed.dart
+++ lib/widgets/text_filed.dart
@@ -1,27 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rocket/flutter_rocket.dart';
 import 'package:integration_flutter_rocket/constants/texts.dart';
 
 class TextFielld extends StatelessWidget {
   final String labelText;
-  TextFielld({super.key, required this.labelText});
-  final ValueNotifier<bool> isVisibilty = ValueNotifier(true);
+  final String? Function(String?) validate;
+  TextFielld({
+    super.key,
+    required this.labelText,
+    required this.validate,
+  });
+  final RocketValue<bool> isVisibilty = true.mini;
   @override
   Widget build(BuildContext context) {
     final bool isPassword = labelText == AppTexts.labelPassword;
-    return ValueListenableBuilder(
-        valueListenable: isVisibilty,
-        builder: (context, value, _) {
+    return RocketMiniView(
+        value: isVisibilty,
+        builder: () {
           return TextFormField(
-            obscureText: isPassword ? isVisibilty.value : false,
+            keyboardType: isPassword
+                ? TextInputType.visiblePassword
+                : TextInputType.emailAddress,
+            obscureText: isPassword ? isVisibilty.v : false,
             decoration: InputDecoration(
                 border: const OutlineInputBorder(),
                 labelText: labelText,
                 suffixIcon: isPassword
                     ? IconButton(
                         onPressed: () {
-                          isVisibilty.value = !isVisibilty.value;
+                          isVisibilty.v = !isVisibilty.v;
                         },
-                        icon: Icon(isVisibilty.value
+                        icon: Icon(isVisibilty.v
                             ? Icons.visibility_off
                             : Icons.visibility))
                     : null,
@@ -29,6 +38,7 @@ class TextFielld extends StatelessWidget {
                   isPassword ? Icons.password : Icons.email,
                   size: 22,
                 )),
+            validator: validate,
           );
         });
   }
