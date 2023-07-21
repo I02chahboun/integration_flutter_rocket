7dcec3002b515a35aa5b7ed3d3e9c2a43290e0c8
I02chahboun <ismail02.dev@gmail.com>
2023-07-21 23:47:55 +0200

    add colors

--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -3,4 +3,10 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color green = Color(0xff008000);
   static const Color grey = Color(0xff808080);
+  static const Color red = Color(0xffff0000);
+  static const Color beig = Color(0xffD7DBDD);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color trans = Colors.transparent;
+  static Color grey9 = Colors.grey.shade900;
+  static const Color blue = Color(0xff2874A6);
 }
