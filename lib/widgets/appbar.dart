596e39ecbf850454d7768b3d2d683065c23941cc
I02chahboun <ismail02.dev@gmail.com>
2023-07-21 23:49:04 +0200

    add leading icon & improve color

--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:integration_flutter_rocket/constants/color.dart';
+import 'package:integration_flutter_rocket/constants/texts.dart';
+import 'package:integration_flutter_rocket/constants/util.dart';
 
 class AppBarr extends StatelessWidget implements PreferredSizeWidget {
   final BuildContext context;
@@ -9,21 +12,47 @@ class AppBarr extends StatelessWidget implements PreferredSizeWidget {
       MediaQuery.of(context).size.height * 0.06);
   @override
   Widget build(BuildContext context) {
+    final bool isProductDetails = title == AppTexts.product;
     return AppBar(
       automaticallyImplyLeading: false,
+      shadowColor: isProductDetails ? AppColors.trans : null,
+      backgroundColor: isProductDetails ? AppColors.trans : null,
+      foregroundColor: isProductDetails ? AppColors.trans : null,
+      surfaceTintColor: isProductDetails ? AppColors.trans : null,
+      leading: isProductDetails
+          ? IconButton(
+              onPressed: () {
+                context.pop();
+              },
+              icon: Icon(
+                Icons.arrow_back_ios,
+                size: 20,
+                color: AppColors.grey9,
+              ),
+            )
+          : null,
       title: Text(
         title,
-        style: const TextStyle(fontWeight: FontWeight.w700),
+        style: TextStyle(
+          fontWeight: FontWeight.w700,
+          color: AppColors.grey9,
+        ),
       ),
       centerTitle: true,
       actions: [
         IconButton(
           onPressed: () {},
-          icon: const Icon(Icons.search),
+          icon: Icon(
+            Icons.search,
+            color: AppColors.grey9,
+          ),
         ),
         IconButton(
           onPressed: () {},
-          icon: const Icon(Icons.shopping_cart),
+          icon: Icon(
+            Icons.shopping_cart,
+            color: AppColors.grey9,
+          ),
         ),
       ],
     );
