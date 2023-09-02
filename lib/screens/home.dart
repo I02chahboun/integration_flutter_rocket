5cf6730d3e45bb57ca3426c7950023ee3a0509ea
I02chahboun <ismail02.dev@gmail.com>
2023-09-02 04:03:45 +0200

    check user is login

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rocket/flutter_rocket.dart';
 import 'package:integration_flutter_rocket/constants/texts.dart';
+import 'package:integration_flutter_rocket/constants/util.dart';
 import 'package:integration_flutter_rocket/models/product.dart';
+import 'package:integration_flutter_rocket/screens/login.dart';
 import 'package:integration_flutter_rocket/services/rocket_requests.dart';
 import 'package:integration_flutter_rocket/widgets/appbar.dart';
 import 'package:integration_flutter_rocket/widgets/card_product.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final Product products = Product();
-  Home({super.key});
+  FirebaseAuth instance = FirebaseAuth.instance;
+  @override
+  void initState() {
+    super.initState();
+    instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        context.push(LogIn());
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +43,9 @@ class Home extends StatelessWidget {
             model: products,
             loader: const CircularProgressIndicator(),
             builder: (context, state) {
+              if (state == RocketState.loading) {
+                return const Center(child: CircularProgressIndicator());
+              }
               return GridView.builder(
                 itemCount: products.all!.length,
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
